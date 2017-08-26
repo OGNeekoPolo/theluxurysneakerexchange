32cd3aad30332f2ba8fd9b7eb7a28f3e4112d338
OGNeekoPolo <nktrabue@gmail.com>
2017-08-26 16:41:00 -0400

    We Almost Live

---
+++ app/overrides/logo.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(virtual_path: "spree/shared/header",
+                     replace: "figure#logo",
+                     text: "<h1>TLSE</h1>",
+                     name: "tlse_logo")
