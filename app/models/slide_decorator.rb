c716a5f421435304b6bd4d0288b69c5e01741f40
OGNeekoPolo <nktrabue@gmail.com>
2017-09-19 13:09:23 -0400

    Bug Fix

--- app/models/slide_decorator.rb
+++ app/models/slide_decorator.rb
@@ -8,7 +8,7 @@ Spree::Slide.class_eval do
                         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
                         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
                       },
-                      :s3_region => ENV['AWS_REGION']
+                      :s3_region => ENV['AWS_REGION'],
                       url: ":s3_domain_url",
                       path: '/spree/slides/:id/:style/:basename.:extension',
                       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
